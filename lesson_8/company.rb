cb5497d13ebc646f96dfea38ca80684cce944fa7
agibalovsu <agibalovsu@mail.ru>
2021-10-05 01:38:29 +0300

    add game 2

--- lesson_8/company.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Company
-  attr_accessor :name_company
-end
--- lesson_8/passenger_train.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'train'
-
-class PassengerTrain < Train
-  def initialize(number)
-    super(number, _type = 'passenger')
-  end
-end
